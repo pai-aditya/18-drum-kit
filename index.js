d42af0d2c9dde0194c54898afa8671349da67afb
Aditya Pai <135251159+pai-aditya@users.noreply.github.com>
2023-06-14 00:37:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+var n = document.querySelectorAll(".drum").length;
+
+for(var i=0; i<n; i++)
+{
+     document.querySelectorAll(".drum")[i].addEventListener("click",function (){
+
+          var buttonInnerHTML = this.innerHTML;
+          makeSound(buttonInnerHTML);
+          showButtonAnimation(buttonInnerHTML);
+     });
+}
+
+document.addEventListener("keydown",function(event){
+
+     makeSound(event.key);
+     showButtonAnimation(event.key);
+
+});
+
+function makeSound(key){
+     
+     switch (key) {
+          case "a": var tom1 = new Audio("sounds/tom-1.mp3");
+                    tom1.play();
+          break;
+
+          case "s": var tom2 = new Audio("sounds/tom-2.mp3");
+                    tom2.play();
+          break;
+          
+          case "d": var tom3 = new Audio("sounds/tom-3.mp3");
+                    tom3.play();
+          break;
+
+          case "f": var tom4 = new Audio("sounds/tom-4.mp3");
+                    tom4.play();
+          break;
+
+          case "j": var crash = new Audio("sounds/crash.mp3");
+                    crash.play();
+          break;
+
+          case "k": var kick = new Audio("sounds/kick-bass.mp3");
+                    kick.play();
+          break;
+
+          case "l": var snare = new Audio("sounds/snare.mp3");
+                    snare.play();
+          break;
+
+          default: console.log(buttonInnerHTML);
+               break;
+     }
+}
+
+function showButtonAnimation(key){
+
+     var activeButton = document.querySelector("."+key);
+     activeButton.classList.add("pressed");
+     
+     setTimeout(function(){
+          activeButton.classList.remove("pressed")
+     },
+     100);
+}
